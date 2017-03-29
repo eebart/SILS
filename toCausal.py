97d308635c3655355205eb6073e0358ad18154be
TRomijn <t.t.romijn@gmail.com>
2017-03-29 11:48:59 +0200

    Some P3.5 updates and indicated some pending issues
    - cPickle is replaced by the newer pickle library
    - Added # TODO for some emaDLL functions

--- toCausal.py
+++ toCausal.py
@@ -6,22 +6,26 @@ Created on Dec 7, 2011
 '''
 import networkx as nx
 import matplotlib.pyplot as plt
-import cPickle
+import pickle
+#TODO
 from expWorkbench.vensimDLLwrapper import VensimWarning
 
+# TODO (not used, not necessary, only for layout of plotting)
 try:
     from networkx import graphviz_layout
 except ImportError:
     raise ImportError("This example needs Graphviz and either PyGraphviz or Pydot")
 
-
+# TODO
 from expWorkbench import vensim
 from expWorkbench import vensimDLLwrapper as venDLL
 
-vensim.load_model(r'D:\epruyt\Desktop\Ageing2.vpm')
+
+model_name = "Your_Model.vpm"
+vensim.load_model(model_name)
 #C:\workspace\EMA-workbench\src\sandbox\sils\MODEL.vpm
 
-
+# TODO
 vars = venDLL.get_varnames()
 
 graph = nx.DiGraph()
@@ -29,13 +33,16 @@ graph.add_nodes_from(vars)
 
 for var in vars:
     try:
+    	# TODO
         causes = venDLL.get_varattrib(var, attribute=4) #cause
         for cause in causes:
             graph.add_edge(cause, var)
     except VensimWarning:
         print var
 
-cPickle.dump(graph, open("Ageing2.cPickle",'wb'))
+
+new_filename = "filename.pickle"
+pickle.dump(graph, open(new_filename,'wb'), pickle.HIGHEST_PROTOCOL) # (Highest protocol is optional)
 print('done')
 #pos=nx.graphviz_layout(graph,prog='twopi',args='')
 #nx.draw_networkx(graph, pos=pos)
